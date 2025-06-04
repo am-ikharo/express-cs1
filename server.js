fef38a971cc82451346d2cec9c3e46dd80c87575
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-06-05 00:19:00 +0100

    created a static server
    created a static server and pointed to the public folder as the static folder

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+//creating a static server
 
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'about.html'));
-});
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+// app.get('/about', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'about.html'));
+// });
 
 
 app.listen(8080, () => console.log(`Server is running on port 8080`));
