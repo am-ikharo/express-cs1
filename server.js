73af1c93a22e9e2904c53ec2f94a2250f7fe886c
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-06-05 00:00:43 +0100

    created a simple server

--- server.js
+++ server.js
@@ -2,5 +2,9 @@ const express = require('express')
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send({message: 'Hello world'});
+});
+
 
 app.listen(8080, () => console.log(`Server is running on port 8080`));
